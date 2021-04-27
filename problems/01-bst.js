775c7e3a9c2ce67475437843ceca6a9b4467aef1
Joe Kim <joekim6a@gmail.com>
2021-04-27 09:36:38 -0700

    solved insert on problem 01

--- problems/01-bst.js
+++ problems/01-bst.js
@@ -9,13 +9,57 @@ class TreeNode {
 class BST {
     // Initialize an empty binary search tree
     constructor() {
-        // Your code here
+        this.root = null;
     }
 
     // Insert a node into the binary search tree
     insert(val, currentNode=this.root) {
-        // Your code here
+        const newNode = new TreeNode(val);
+        if (!currentNode) {
+            this.root = newNode;
+            return;
+        }
+
+        while (currentNode) {
+            if (newNode.val >= currentNode.val) {
+                if (currentNode.right) {
+                    currentNode = currentNode.right;
+                } else {
+                    currentNode.right = newNode;
+                    return;
+                }
+            } else if (newNode.val < currentNode.val) {
+                if (currentNode.left) {
+                    currentNode = currentNode.left;
+                } else {
+                    currentNode.left = newNode;
+                    return;
+                }
+            }
+        }
     }
+
+// ALTERNATIVE SOLUTION THAT WORKS TOO, BUT USES RECURISON INSTEAD OF ITERATION
+    // insert(val, currentNode=this.root) {
+    //     if (!currentNode) {
+    //         this.root = new TreeNode(val);
+    //         return;
+    //     }
+
+    //     if (newNode.val >= currentNode.val) {
+    //         if (currentNode.right) {
+    //             this.insert(val, currentNode.right);
+    //         } else {
+    //             currentNode.right = new TreeNode(val);
+    //         }
+    //     } else if (newNode.val < currentNode.val) {
+    //         if (currentNode.left) {
+    //             this.insert(val, currentNode.left);
+    //         } else {
+    //             currentNode.left = new TreeNode(val);
+    //         }
+    //     }
+    // }
 
     // Perform a recursive search through the binary search tree
     searchRecur(val, currentNode=this.root) {
